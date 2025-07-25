ffdaa0bb8514d7061b39943980371770f61dfe2e
analine.hernandez.arias <analine.hernandez.arias@bbva.com>
2025-07-25 11:47:39 -0600

    Agrego action yml e index.json

---
+++ index.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core');
+try {
+    const name = core.getInput('name');
+    console.log(`Hola, ${name}!`);
+} catch (error) {
+    core.setFailed(`Error: ${error.message}`);
+    
+}
